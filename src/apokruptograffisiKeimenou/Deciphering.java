27f21275f5b08c8448f64729b59f6235cd46cb71
Ioanna-Nikoletta Vasili <IVasili@atc.gr>
2023-12-11 10:42:43 +0200

    commit

---
+++ src/apokruptograffisiKeimenou/Deciphering.java
@@ -0,0 +1,44 @@
+package apokruptograffisiKeimenou;
+
+public class Deciphering {
+
+	public String decipher(String cipher_text) {
+		String decipher_text = null;
+		int offset = 10;
+		char cipher_text_char_array[] = cipher_text.toCharArray();
+		
+		for(int i=0; i<cipher_text_char_array.length; i++) {
+			char current_char = cipher_text_char_array[i];
+			if (current_char == ' ') continue;
+			
+			int originalAlphabetPosition = current_char - 'a';
+			
+			int newAlphabetPosition = (originalAlphabetPosition - offset + 26) % 26;
+			
+			char newCharacter = (char) ('a' + newAlphabetPosition);
+			
+			cipher_text_char_array[i] = newCharacter;	
+		}
+		
+		decipher_text = String.valueOf(cipher_text_char_array);
+		return decipher_text;
+	}
+
+	public String cipher(String normal_text, int offset) {
+		String cipher_text = null;
+		char normal_text_array[] = normal_text.toCharArray();
+		
+		for(int i=0; i<normal_text_array.length; i++) {
+			char current_char = normal_text_array[i];
+			if (current_char == ' ') continue;
+			
+			int originalAlphabetPosition = current_char - 'a';
+			int newAlphabetPosition = (originalAlphabetPosition + offset)%26;
+			char newCharacter = (char) ('a' + newAlphabetPosition);
+			
+			normal_text_array[i] = newCharacter;	
+		}
+		cipher_text = String.valueOf(normal_text_array);
+		return cipher_text;
+	}
+}
---
+++ src/apokruptograffisiKeimenou/Main.java
@@ -0,0 +1,21 @@
+package apokruptograffisiKeimenou;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Deciphering d = new Deciphering();
+		String cipher = "ro dyvn wo s myevn xofob dokmr k vvkwk dy nbsfo";
+		String deciphered = d.decipher(cipher);
+		System.out.println("deciphered: "+deciphered);
+		
+		String cipher_validate = d.cipher(deciphered, 10);
+		System.out.println("cipher(again): "+cipher_validate);
+		
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		System.out.println("cipher text: " + d.cipher(line, 5));
+	}
+
+}
